f76cf59fbf67c4c6dc89076d6207e38db41b4906
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-19 00:32:58 +0300

    Added by random mottos parser; jsoup integration

--- app/src/main/java/github/karchx/motto/ui/home/HomeViewModel.kt
+++ app/src/main/java/github/karchx/motto/ui/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package github.karchx.motto.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import github.karchx.motto.search_engine.citaty_info_website.parsers.ByRandomMottosParser
 
 class HomeViewModel : ViewModel() {
 
