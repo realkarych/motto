87b62f068198152c414dbed021153f33ac776493
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-18 23:53:37 +0300

    Added dataclasses; interface for parsers

---
+++ app/src/main/java/github/karchx/motto/search_engine/citaty_info_website/data/Author.kt
@@ -0,0 +1,8 @@
+package github.karchx.motto.search_engine.citaty_info_website.data
+
+data class Author(
+    val uniqueID: String,
+    val name: String,
+    val surname: String,
+    val pageUri: String
+)
---
+++ app/src/main/java/github/karchx/motto/search_engine/citaty_info_website/data/Topic.kt
@@ -0,0 +1,7 @@
+package github.karchx.motto.search_engine.citaty_info_website.data
+
+data class Topic(
+    val uniqueID: String,
+    val title: String,
+    val topicUri: String
+)
