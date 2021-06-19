47bb6a608c6e719a1068833ca8486090f122f3e3
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-19 19:46:29 +0300

    Added onClickListener for mottos

--- app/src/main/java/github/karchx/motto/search_engine/citaty_info_website/data/Constants.kt
+++ app/src/main/java/github/karchx/motto/search_engine/citaty_info_website/data/Constants.kt
@@ -7,7 +7,7 @@ class Constants {
         const val REQUEST_SEARCH_TYPE = "search/site/"
         const val ARTICLE_ROOT_ELEMENT_NAME = "article"
         const val MOTTOS_SORT_TYPE = "?sort_by=rating"
-        const val QUANTITY_MOTTOS_IN_LIST = 5
+        const val QUANTITY_MOTTOS_IN_LIST = 12
         const val QUANTITY_WORDS_IN_MOTTO_TITLE = 12
     }
 }
