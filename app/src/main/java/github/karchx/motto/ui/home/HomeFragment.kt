4a11bdd2310069ff33a2af66a32315092966a176
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-19 19:46:29 +0300

    Added onClickListener for mottos

--- app/src/main/java/github/karchx/motto/ui/home/HomeFragment.kt
+++ app/src/main/java/github/karchx/motto/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package github.karchx.motto.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,12 +13,20 @@ import androidx.recyclerview.widget.RecyclerView
 import github.karchx.motto.databinding.FragmentHomeBinding
 import github.karchx.motto.search_engine.citaty_info_website.data.Motto
 import github.karchx.motto.ui.home.adapters.RandomMottosRecyclerAdapter
+import github.karchx.motto.ui.listeners.OnClickMottoItemListener
 
 class HomeFragment : Fragment() {
 
+    // Data
+    private lateinit var mottos: ArrayList<Motto>
+
+    // Views
     private lateinit var mRandomMottosRecycler: RecyclerView
 
+    // ViewModels
     private lateinit var homeViewModel: HomeViewModel
+
+    //Bindings
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
@@ -35,9 +44,21 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initViews()
 
-        homeViewModel.randomMottos.observe(viewLifecycleOwner, Observer { mottos ->
+        homeViewModel.randomMottos.observe(viewLifecycleOwner, Observer {
+            mottos = it
             displayMottosRecycler(mottos)
         })
+
+        mRandomMottosRecycler.addOnItemTouchListener(
+            OnClickMottoItemListener(requireContext(), object :
+                OnClickMottoItemListener.OnItemClickListener {
+                override fun onItemClick(view: View, position: Int) {
+                    val clickedMotto = mottos[position]
+                    Log.d("posmotto", clickedMotto.toString())
+                }
+            })
+        )
+
     }
 
     override fun onDestroyView() {
