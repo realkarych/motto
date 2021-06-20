e3c3949cc145399a212d37531d00bebea33030c1
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-20 22:34:25 +0300

    Added authors and topics recycler

---
+++ app/src/main/java/github/karchx/motto/ui/dashboard/adapters/DashboardLayoutAdapter.kt
@@ -0,0 +1,20 @@
+package github.karchx.motto.ui.dashboard.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import github.karchx.motto.storages.Constants
+import github.karchx.motto.ui.dashboard.DashboardFragment
+
+class DashboardLayoutAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        val fragment = DashboardFragment()
+        fragment.arguments = Bundle().apply {
+            putString(Constants.KEYWORD_MOTTO_TYPE, Constants.MOTTO_TYPES_NAMES[position])
+        }
+        return fragment
+    }
+}
