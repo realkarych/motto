c714e8e7dae781f5a66181994540fcc063a665c1
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-20 22:44:17 +0300

    Refactoring

--- app/src/main/java/github/karchx/motto/storages/Constants.kt
+++ app/src/main/java/github/karchx/motto/storages/Constants.kt
@@ -11,7 +11,8 @@ class Constants {
         const val QUANTITY_MOTTOS_IN_LIST = 24
         const val QUANTITY_WORDS_IN_MOTTO_TITLE = 12
         val MOTTO_TYPES_NAMES = arrayListOf("Authors", "Topics")
-        val MOTTO_TYPES_ICONS = arrayListOf(R.drawable.ic_baseline_person_24, R.drawable.ic_baseline_topic_24)
+        val MOTTO_TYPES_ICONS =
+            arrayListOf(R.drawable.ic_baseline_person_24, R.drawable.ic_baseline_topic_24)
         const val KEYWORD_MOTTO_TYPE = "motto_type"
     }
 }
--- app/src/main/java/github/karchx/motto/ui/home/HomeViewModel.kt
+++ app/src/main/java/github/karchx/motto/ui/home/HomeViewModel.kt
@@ -3,9 +3,9 @@ package github.karchx.motto.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import github.karchx.motto.storages.Constants
 import github.karchx.motto.search_engine.citaty_info_website.data.Motto
 import github.karchx.motto.search_engine.citaty_info_website.parsers.ByRandomMottosParser
+import github.karchx.motto.storages.Constants
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
