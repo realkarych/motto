3a426e836f7a3372d3e668ddc85e260886f47cc8
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-20 10:12:50 +0300

    Uses Editor's Choice instead of random mottos. Parser optimizations

--- app/src/main/java/github/karchx/motto/search_engine/citaty_info_website/data/Constants.kt
+++ app/src/main/java/github/karchx/motto/search_engine/citaty_info_website/data/Constants.kt
@@ -3,11 +3,10 @@ package github.karchx.motto.search_engine.citaty_info_website.data
 class Constants {
     companion object {
         const val DOMAIN = "https://citaty.info/"
-        const val RANDOM_SEARCH_TYPE = "random/"
         const val REQUEST_SEARCH_TYPE = "search/site/"
         const val ARTICLE_ROOT_ELEMENT_NAME = "article"
         const val MOTTOS_SORT_TYPE = "?sort_by=rating"
-        const val QUANTITY_MOTTOS_IN_LIST = 12
+        const val QUANTITY_MOTTOS_IN_LIST = 24
         const val QUANTITY_WORDS_IN_MOTTO_TITLE = 12
     }
 }
