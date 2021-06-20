a4ed05c68ad77f538424da4a0873a7d5ed0a1007
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-20 22:34:25 +0300

    Added authors and topics recycler

--- app/src/main/java/github/karchx/motto/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/github/karchx/motto/ui/dashboard/DashboardFragment.kt
@@ -4,19 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import github.karchx.motto.R
 import github.karchx.motto.databinding.FragmentDashboardBinding
+import github.karchx.motto.search_engine.citaty_info_website.data.Author
+import github.karchx.motto.search_engine.citaty_info_website.data.Topic
+import github.karchx.motto.storages.Constants
+import github.karchx.motto.ui.dashboard.adapters.AuthorsRecyclerAdapter
+import github.karchx.motto.ui.dashboard.adapters.TopicsRecyclerAdapter
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private lateinit var authors: ArrayList<Author>
+    private lateinit var topics: ArrayList<Topic>
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private lateinit var mAuthorsRecycler: RecyclerView
+    private lateinit var mTopicsRecycler: RecyclerView
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,21 +35,60 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initViews()
+
+        dashboardViewModel.authors.observe(viewLifecycleOwner, Observer { _authors ->
+            authors = _authors
+            arguments?.takeIf { it.containsKey(Constants.KEYWORD_MOTTO_TYPE) }?.apply {
+                // If a section with authors is open
+                if (getString(Constants.KEYWORD_MOTTO_TYPE) == resources.getString(R.string.authors)) {
+                    setAuthorsRecycler(authors)
+                }
+            }
         })
-        return root
+        dashboardViewModel.topics.observe(viewLifecycleOwner, Observer { _topics ->
+            topics = _topics
+            arguments?.takeIf { it.containsKey(Constants.KEYWORD_MOTTO_TYPE) }?.apply {
+                // If a section with authors is open
+                if (getString(Constants.KEYWORD_MOTTO_TYPE) == resources.getString(R.string.topics)) {
+                    setTopicsRecycler(topics)
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setAuthorsRecycler(authors: ArrayList<Author>) {
+        val layoutManager = GridLayoutManager(context, 2)
+        val adapter = AuthorsRecyclerAdapter(this@DashboardFragment, authors)
+
+        mAuthorsRecycler.setHasFixedSize(true)
+        mAuthorsRecycler.layoutManager = layoutManager
+        mAuthorsRecycler.adapter = adapter
+    }
+
+    private fun setTopicsRecycler(topics: ArrayList<Topic>) {
+        val layoutManager = GridLayoutManager(context, 2)
+        val adapter = TopicsRecyclerAdapter(this@DashboardFragment, topics)
+
+        mTopicsRecycler.setHasFixedSize(true)
+        mTopicsRecycler.layoutManager = layoutManager
+        mTopicsRecycler.adapter = adapter
+    }
+
+    private fun initViews() {
+        mAuthorsRecycler = binding.recyclerviewAuthorsDashboard
+        mTopicsRecycler = binding.recyclerviewTopicsDashboard
     }
 }
--- app/src/main/java/github/karchx/motto/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/github/karchx/motto/ui/dashboard/DashboardViewModel.kt
@@ -3,11 +3,23 @@ package github.karchx.motto.ui.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import github.karchx.motto.search_engine.citaty_info_website.data.Author
+import github.karchx.motto.search_engine.citaty_info_website.data.Topic
+import github.karchx.motto.storages.AuthorsStorage
+import github.karchx.motto.storages.TopicsStorage
 
 class DashboardViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    private val authorsStorage = AuthorsStorage()
+    private val topicsStorage = TopicsStorage()
+
+    private val _authors = MutableLiveData<ArrayList<Author>>().apply {
+        value = authorsStorage.getAuthors()
     }
-    val text: LiveData<String> = _text
+    private val _topics = MutableLiveData<ArrayList<Topic>>().apply {
+        value = topicsStorage.getTopics()
+    }
+
+    val authors: LiveData<ArrayList<Author>> = _authors
+    val topics: LiveData<ArrayList<Topic>> = _topics
 }
