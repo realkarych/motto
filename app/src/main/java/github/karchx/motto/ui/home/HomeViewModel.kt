f1a1fb3ca173015e73f6f795e09f8f61c7bccd40
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-22 11:49:25 +0300

    Added global scope searching

--- app/src/main/java/github/karchx/motto/ui/home/HomeViewModel.kt
+++ app/src/main/java/github/karchx/motto/ui/home/HomeViewModel.kt
@@ -3,8 +3,11 @@ package github.karchx.motto.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import github.karchx.motto.search_engine.citaty_info_website.data.Author
 import github.karchx.motto.search_engine.citaty_info_website.data.Motto
+import github.karchx.motto.search_engine.citaty_info_website.parsers.ByAuthorMottosParser
 import github.karchx.motto.search_engine.citaty_info_website.parsers.ByRandomMottosParser
+import github.karchx.motto.search_engine.citaty_info_website.parsers.ByRequestMottosParser
 import github.karchx.motto.storages.Constants
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -18,6 +21,13 @@ class HomeViewModel : ViewModel() {
             putRandomMottosPostValue()
         }
     }
+    private val _requestMottos = MutableLiveData<ArrayList<Motto>>().apply {}
+    fun putRequestMottosPostValue(request: String) {
+        val parser = ByRequestMottosParser(request)
+        GlobalScope.launch {
+            _requestMottos.postValue(parser.getMottos(Constants.QUANTITY_MOTTOS_IN_LIST))
+        }
+    }
 
     fun putRandomMottosPostValue() {
         GlobalScope.launch {
@@ -26,4 +36,5 @@ class HomeViewModel : ViewModel() {
     }
 
     val randomMottos: LiveData<ArrayList<Motto>> = _randomMottos
+    val requestMottos: LiveData<ArrayList<Motto>> = _requestMottos
 }
