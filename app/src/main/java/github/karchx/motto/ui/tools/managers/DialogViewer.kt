66a948490da970f66c1f87019e9003891a9ffbdf
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-24 15:05:08 +0300

    Added saving mottos to favourites (ONLY RANDOM MOTTOS!!!)

--- app/src/main/java/github/karchx/motto/ui/tools/managers/DialogViewer.kt
+++ app/src/main/java/github/karchx/motto/ui/tools/managers/DialogViewer.kt
@@ -1,23 +1,59 @@
 package github.karchx.motto.ui.tools.managers
 
 import android.app.Dialog
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
 import github.karchx.motto.R
 import github.karchx.motto.search_engine.citaty_info_website.data.Motto
+import github.karchx.motto.model.db.Motto as dbMotto
 
 class DialogViewer {
     companion object {
-        fun displayFullMottoDialog(dialog: Dialog, clickedMotto: Motto) {
+        fun displayFullMottoDialog(
+            context: Context,
+            dialog: Dialog,
+            clickedMotto: Motto,
+            allDbMottos: List<dbMotto>
+        ) {
             dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             dialog.setCancelable(true)
             dialog.show()
 
             val tvFullMottoQuote = dialog.findViewById<TextView>(R.id.textview_motto_full_quote)
             val tvFullMottoSource = dialog.findViewById<TextView>(R.id.textview_motto_full_source)
+            val ivAddToFavourites = dialog.findViewById<ImageView>(R.id.imageview_is_saved_motto)
             tvFullMottoQuote.text = clickedMotto.quote
             tvFullMottoSource.text = clickedMotto.source
+
+            val allQuotes = ArrayList<String>()
+            val allSources = ArrayList<String>()
+            for (motto in allDbMottos) {
+                allQuotes.add(motto.quote)
+                allSources.add(motto.source)
+            }
+
+            if (clickedMotto.quote in allQuotes && clickedMotto.source in allSources) {
+                ivAddToFavourites.setImageDrawable(
+                    AppCompatResources.getDrawable(
+                        context,
+                        R.drawable.ic_favorite_white
+                    )
+                )
+                ivAddToFavourites.visibility = View.VISIBLE
+            } else {
+                ivAddToFavourites.setImageDrawable(
+                    AppCompatResources.getDrawable(
+                        context,
+                        R.drawable.ic_favorite_border_white
+                    )
+                )
+                ivAddToFavourites.visibility = View.VISIBLE
+            }
         }
     }
 }
