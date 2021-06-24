e4cd684f318c96da15c9c0e122466aa15452d9c0
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-24 18:51:00 +0300

    Added motto deleting, opening functuanality in favourites fragment

--- app/src/main/java/github/karchx/motto/ui/favourites/FavouritesFragment.kt
+++ app/src/main/java/github/karchx/motto/ui/favourites/FavouritesFragment.kt
@@ -1,29 +1,44 @@
 package github.karchx.motto.ui.favourites
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import github.karchx.motto.R
 import github.karchx.motto.databinding.FragmentFavouritesBinding
 import github.karchx.motto.model.db.Motto
-import github.karchx.motto.ui.tools.adapters.AuthorsRecyclerAdapter
 import github.karchx.motto.ui.tools.adapters.FromDbMottosRecyclerAdapter
+import github.karchx.motto.ui.tools.listeners.OnClickAddToFavouritesListener
+import github.karchx.motto.ui.tools.listeners.OnClickRecyclerItemListener
+import github.karchx.motto.ui.tools.managers.Copier
+import github.karchx.motto.ui.tools.managers.DialogViewer
+import github.karchx.motto.ui.tools.managers.Toaster
 import github.karchx.motto.viewmodels.FavouritesViewModel
 import github.karchx.motto.viewmodels.MottosViewModel
+import github.karchx.motto.search_engine.citaty_info_website.data.Motto as parsedMotto
 
 class FavouritesFragment : Fragment() {
 
     private lateinit var favouritesViewModel: FavouritesViewModel
     private lateinit var mottosViewModel: MottosViewModel
-    
+    private lateinit var mFullMottoDialog: Dialog
+    private lateinit var mFullMottoCardView: CardView
+
     private lateinit var mFavouriteMottosRecycler: RecyclerView
+    private lateinit var mAddToFavouritesImageView: ImageView
 
     private var _binding: FragmentFavouritesBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var savedMottos: List<Motto>
+    private lateinit var clickedMotto: Motto
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,18 +52,74 @@ class FavouritesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initViews()
         initData()
-        mottosViewModel.allMottos.observe(viewLifecycleOwner) {
-            val layoutManager = GridLayoutManager(context, 1)
-            val adapter = FromDbMottosRecyclerAdapter(it.reversed() as ArrayList<Motto>)
-            mFavouriteMottosRecycler.setHasFixedSize(true)
-            mFavouriteMottosRecycler.layoutManager = layoutManager
-            mFavouriteMottosRecycler.adapter = adapter
+
+        observeDbMottos()
+        setAddToFavouritesBtnClickListener()
+        setFavouriteMottosClickListener()
+        setFullMottoCardViewClickListener()
+    }
+
+    private fun observeDbMottos() {
+        mottosViewModel.allMottos.observe(viewLifecycleOwner) { mottos ->
+            savedMottos = mottos.reversed()
+            displayFavouriteMottos(savedMottos)
+        }
+    }
+
+    private fun setFavouriteMottosClickListener() {
+        mFavouriteMottosRecycler.addOnItemTouchListener(
+            OnClickRecyclerItemListener(requireContext(), object :
+                OnClickRecyclerItemListener.OnItemClickListener {
+                override fun onItemClick(view: View, position: Int) {
+                    clickedMotto = savedMottos[position]
+                    DialogViewer.displayFullMottoDialog(
+                        requireContext(),
+                        mFullMottoDialog,
+                        parsedMotto(clickedMotto.quote, clickedMotto.source, false),
+                        savedMottos
+                    )
+                    observeDbMottos()
+                }
+            })
+        )
+    }
+
+    private fun setFullMottoCardViewClickListener() {
+        mFullMottoCardView.setOnClickListener {
+            val text = Copier.getMottoDataToCopy(
+                context = requireContext(),
+                quote = clickedMotto.quote,
+                source = clickedMotto.source
+            )
+
+            Copier.copyText(requireActivity(), text)
+            Toaster.displayTextIsCopiedToast(requireContext())
+        }
+    }
+
+    private fun setAddToFavouritesBtnClickListener() {
+        mAddToFavouritesImageView.setOnClickListener {
+            OnClickAddToFavouritesListener.handleMotto(
+                requireContext(),
+                mottosViewModel,
+                mAddToFavouritesImageView,
+                savedMottos,
+                parsedMotto(clickedMotto.quote, clickedMotto.source, false)
+            )
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun displayFavouriteMottos(mottos: List<Motto>) {
+        val layoutManager = GridLayoutManager(context, 1)
+        val adapter = FromDbMottosRecyclerAdapter(mottos as ArrayList<Motto>)
+        mFavouriteMottosRecycler.setHasFixedSize(true)
+        mFavouriteMottosRecycler.layoutManager = layoutManager
+        mFavouriteMottosRecycler.adapter = adapter
     }
 
     private fun initData() {
@@ -59,8 +130,12 @@ class FavouritesFragment : Fragment() {
             ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
         ).get(MottosViewModel(application = requireActivity().application)::class.java)
     }
-    
+
     private fun initViews() {
         mFavouriteMottosRecycler = binding.recyclerviewFavouriteMottos
+        mFullMottoDialog = Dialog(requireActivity())
+        mFullMottoDialog.setContentView(R.layout.dialog_full_motto)
+        mFullMottoCardView = mFullMottoDialog.findViewById(R.id.cardview_full_motto_item)
+        mAddToFavouritesImageView = mFullMottoDialog.findViewById(R.id.imageview_is_saved_motto)
     }
 }
