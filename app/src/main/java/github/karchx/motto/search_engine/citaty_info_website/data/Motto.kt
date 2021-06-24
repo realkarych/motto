97054338201331dcfe9caa3f79a9804d4990f007
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-24 21:46:13 +0300

    Removed isSaved arg from Motto dataclass

--- app/src/main/java/github/karchx/motto/search_engine/citaty_info_website/data/Motto.kt
+++ app/src/main/java/github/karchx/motto/search_engine/citaty_info_website/data/Motto.kt
@@ -2,6 +2,5 @@ package github.karchx.motto.search_engine.citaty_info_website.data
 
 data class Motto(
     val quote: String,
-    val source: String,
-    val isSaved: Boolean
+    val source: String
 )
