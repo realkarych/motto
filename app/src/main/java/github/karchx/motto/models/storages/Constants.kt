cb5c878572c66f8746917e6041981d69f567ffa0
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-06-25 12:39:50 +0300

    Layout improvements

--- app/src/main/java/github/karchx/motto/models/storages/Constants.kt
+++ app/src/main/java/github/karchx/motto/models/storages/Constants.kt
@@ -8,7 +8,7 @@ class Constants {
         const val REQUEST_SEARCH_TYPE = "search/site/"
         const val ARTICLE_ROOT_ELEMENT_NAME = "article"
         const val MOTTOS_SORT_TYPE = "?sort_by=rating"
-        const val QUANTITY_MOTTOS_IN_LIST = 40
+        const val QUANTITY_MOTTOS_IN_LIST = 64
         const val QUANTITY_WORDS_IN_MOTTO_TITLE = 12
         val MOTTO_TYPES_NAMES = arrayListOf("Authors", "Topics")
         val MOTTO_TYPES_ICONS =
