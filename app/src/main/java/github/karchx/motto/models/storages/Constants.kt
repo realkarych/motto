f93acb9970b92f77be5d186d783639089c3fda15
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-07-02 14:11:14 +0300

    Added content to storages

--- app/src/main/java/github/karchx/motto/models/storages/Constants.kt
+++ app/src/main/java/github/karchx/motto/models/storages/Constants.kt
@@ -8,7 +8,7 @@ class Constants {
         const val DOMAIN = "https://citaty.info/"
         const val REQUEST_SEARCH_TYPE = "search/site/"
         const val ARTICLE_ROOT_ELEMENT_NAME = "article"
-        const val MOTTOS_SORT_TYPE = "?sort_by=rating"
+        const val MOTTOS_SORT_TYPE = "?sort_by=rating&page=1"
         const val QUANTITY_MOTTOS_IN_LIST = 64
         const val QUANTITY_WORDS_IN_MOTTO_TITLE = 12
 
--- app/src/main/java/github/karchx/motto/models/storages/TVChannelsStorage.kt
+++ app/src/main/java/github/karchx/motto/models/storages/TVChannelsStorage.kt
@@ -1,4 +1,25 @@
 package github.karchx.motto.models.storages
 
+import github.karchx.motto.search_engine.citaty_info_website.data.TVChannel
+
 class TVChannelsStorage {
+
+    fun getChannels(): ArrayList<TVChannel> {
+        val channels = ArrayList<TVChannel>()
+
+        channels.add(TVChannel("stand_up", "Stand Up", "/tv/stand-up"))
+        channels.add(TVChannel("versus_battle", "Versus Battle", "/tv/versus-battle"))
+        channels.add(TVChannel("vdud", "вДудь", "/tv/vdud"))
+        channels.add(TVChannel("improvizaciya", "Импровизация", "/tv/improvizaciya"))
+        channels.add(TVChannel("comedy_club", "Comedy Club", "/tv/comedy-club-kamedi-klab"))
+        channels.add(TVChannel("kvn", "КВН", "/tv/kvn"))
+        channels.add(TVChannel("frenki_shou", "Фрэнки-шоу", "/tv/frenki-shou"))
+        channels.add(TVChannel("chto_bylo_dalshe", "Что было дальше?", "/tv/chto-bylo-dalshe"))
+        channels.add(TVChannel("100500", "Макс +100500", "/tv/100500"))
+        channels.add(TVChannel("22_komika", "22 комика", "/tv/22-komika"))
+        channels.add(TVChannel("60_minut", "60 минут", "/tv/60-minut"))
+        channels.add(TVChannel("comedy_woman", "Comedy Woman", "/tv/comedy-woman"))
+
+        return channels
+    }
 }
