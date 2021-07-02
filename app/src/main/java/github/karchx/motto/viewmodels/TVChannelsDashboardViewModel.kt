46d5e64a129098db58977f9f7a6c97316c3a795f
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-07-03 01:03:20 +0300

    Added channels & series viewModels

---
+++ app/src/main/java/github/karchx/motto/viewmodels/TVChannelsDashboardViewModel.kt
@@ -0,0 +1,33 @@
+package github.karchx.motto.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import github.karchx.motto.models.storages.Constants
+import github.karchx.motto.models.storages.TVChannelsStorage
+import github.karchx.motto.search_engine.citaty_info_website.data.Motto
+import github.karchx.motto.search_engine.citaty_info_website.data.TVChannel
+import github.karchx.motto.search_engine.citaty_info_website.parsers.ByChannelMottosParser
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class TVChannelsDashboardViewModel : ViewModel() {
+
+    private val tvChannelsStorage = TVChannelsStorage()
+
+    private val _tvChannels = MutableLiveData<ArrayList<TVChannel>>().apply {
+        value = tvChannelsStorage.getChannels()
+    }
+
+    private val _tvChannelMottos = MutableLiveData<ArrayList<Motto>>().apply {}
+
+    fun putTVChannelMottosPostValue(channel: TVChannel) {
+        val parser = ByChannelMottosParser(channel)
+        GlobalScope.launch {
+            _tvChannelMottos.postValue(parser.getMottos(Constants.QUANTITY_MOTTOS_IN_LIST))
+        }
+    }
+
+    val tvChannels: LiveData<ArrayList<TVChannel>> = _tvChannels
+    val tvChannelMottos: LiveData<ArrayList<Motto>> = _tvChannelMottos
+}
