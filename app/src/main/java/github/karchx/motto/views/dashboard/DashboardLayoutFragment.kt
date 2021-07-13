a52c0aba6b05f10298bf081dc11f40eb92a28776
AndrusKarchevskiy <karch.andrus@gmail.com>
2021-07-13 15:01:19 +0300

    Back arrow improvements, added displaying last-opened tab & pager item like current

--- app/src/main/java/github/karchx/motto/views/dashboard/DashboardLayoutFragment.kt
+++ app/src/main/java/github/karchx/motto/views/dashboard/DashboardLayoutFragment.kt
@@ -1,9 +1,12 @@
 package github.karchx.motto.views.dashboard
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
@@ -22,6 +25,7 @@ class DashboardLayoutFragment : Fragment() {
 
     private lateinit var viewPager: ViewPager2
     private lateinit var tabLayout: TabLayout
+    private lateinit var sharedPrefs: SharedPreferences
 
 
     override fun onCreateView(
@@ -35,14 +39,33 @@ class DashboardLayoutFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        sharedPrefs = requireActivity().getPreferences(Context.MODE_PRIVATE)
+
         setViewPagerAndTabLayout()
+
+        tabLayout.addOnTabSelectedListener(object :
+            TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val position = tab.position
+                sharedPrefs.edit().putInt(Constants.LAST_TAB_SELECTED, position).apply()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+
+                sharedPrefs.edit().putInt(Constants.LAST_TAB_SELECTED, position).apply()
+            }
+        })
     }
 
     private fun setViewPagerAndTabLayout() {
         initViewPagerAndTabLayout()
 
         val adapter = ViewPagerAdapter(requireActivity() as MainActivity)
-
         viewPager.adapter = adapter
 
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
@@ -53,6 +76,25 @@ class DashboardLayoutFragment : Fragment() {
                 requireContext().theme
             )
         }.attach()
+
+        val currentItem = sharedPrefs.getInt(Constants.LAST_TAB_SELECTED, 0)
+
+        val tab = tabLayout.getTabAt(currentItem)
+        tab!!.select()
+
+        val recyclerView = (viewPager).getChildAt(0)
+        recyclerView.apply {
+            val itemCount = adapter.itemCount
+            if (itemCount >= currentItem) {
+                viewTreeObserver.addOnGlobalLayoutListener(object :
+                    ViewTreeObserver.OnGlobalLayoutListener {
+                    override fun onGlobalLayout() {
+                        viewTreeObserver.removeOnGlobalLayoutListener(this)
+                        viewPager.setCurrentItem(currentItem, false)
+                    }
+                })
+            }
+        }
     }
 
     private fun initViewPagerAndTabLayout() {
