080f060b23c3ba170800962410c2b1ddb5d6c9a3
karchx <karch.andrus@gmail.com>
2021-07-21 23:09:18 +0300

    Added Book dataclass

---
+++ app/src/main/java/github/karchx/motto/search_engine/citaty_info_website/data/Book.kt
@@ -0,0 +1,8 @@
+package github.karchx.motto.search_engine.citaty_info_website.data
+
+data class Book(
+    val uniqueID: String,
+    val title: String,
+    val authorSurname: String,
+    val pageUri: String
+)
