c188857dbb0b3cc3cc2e5b7c026154cf029a83c3
karchx <karch.andrus@gmail.com>
2021-07-25 13:28:52 +0300

    Updated appbar config

--- app/src/main/java/github/karchx/motto/views/settings/SettingsFragment.kt
+++ app/src/main/java/github/karchx/motto/views/settings/SettingsFragment.kt
@@ -2,7 +2,6 @@ package github.karchx.motto.views.settings
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
