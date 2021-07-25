c2d507b5503907609dfc8e9bbaeb1c68509cdcde
karchx <karch.andrus@gmail.com>
2021-07-25 12:38:44 +0300

    Added carcass of settings fragment. Added new architecture for user shared prefs.

---
+++ app/src/main/java/github/karchx/motto/models/user_settings/MottosRandomness.kt
@@ -0,0 +1,6 @@
+package github.karchx.motto.models.user_settings
+
+import android.content.SharedPreferences
+
+class MottosRandomness(prefs: SharedPreferences) {
+}
---
+++ app/src/main/java/github/karchx/motto/views/settings/SettingsFragment.kt
@@ -0,0 +1,51 @@
+package github.karchx.motto.views.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import github.karchx.motto.databinding.FragmentSettingsBinding
+import github.karchx.motto.models.user_settings.UserPrefs
+import github.karchx.motto.views.MainActivity
+import github.karchx.motto.views.tools.managers.Arrow
+
+class SettingsFragment: Fragment() {
+
+    private lateinit var userPrefs: UserPrefs
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Arrow.hideBackArrow(activity as MainActivity)
+        initData()
+        initViews()
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initData() {
+        userPrefs = UserPrefs(requireContext())
+    }
+
+    private fun initViews() {
+
+    }
+}
