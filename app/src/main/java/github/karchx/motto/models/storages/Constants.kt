eab5c3d0f08da4e2f0cb016b21f02b6aa36a39da
karchx <karch.andrus@gmail.com>
2021-07-26 10:48:08 +0300

    Added items

--- app/src/main/java/github/karchx/motto/models/storages/Constants.kt
+++ app/src/main/java/github/karchx/motto/models/storages/Constants.kt
@@ -37,6 +37,8 @@ class Constants {
 
         const val LAST_TAB_SELECTED = "selected_tab"
         const val NUMBER_OF_DISPLAYED_ADS = "num_of_displayed_ads"
-        const val IS_COPY_WITH_AUTHOR = "is_copy_with_author"
+        const val IS_COPY_WITH_SOURCE = "is_copy_with_source"
+        const val IS_SORT_SOURCES_BY_RANDOM = "is_sort_sources_by_random"
+        const val IS_SORT_MOTTOS_BY_RANDOM = "is_sort_mottos_by_random"
     }
 }
