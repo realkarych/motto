5678bc69b2fa8fa56e1666ec5774b65539edfbf2
karchx <karch.andrus@gmail.com>
2021-07-26 10:48:08 +0300

    Added items

--- app/src/main/java/github/karchx/motto/models/user_settings/MottosRandomness.kt
+++ app/src/main/java/github/karchx/motto/models/user_settings/MottosRandomness.kt
@@ -1,6 +1,20 @@
 package github.karchx.motto.models.user_settings
 
 import android.content.SharedPreferences
+import github.karchx.motto.models.storages.Constants
 
-class MottosRandomness(prefs: SharedPreferences) {
+class MottosRandomness(private val prefs: SharedPreferences) {
+
+    fun isRandom(): Boolean {
+        return prefs.getBoolean(Constants.IS_SORT_MOTTOS_BY_RANDOM, false)
+    }
+
+    fun updateState() {
+        val isRandom = MottosRandomness(prefs).isRandom()
+        if (isRandom) {
+            prefs.edit().putBoolean(Constants.IS_SORT_MOTTOS_BY_RANDOM, false).apply()
+        } else {
+            prefs.edit().putBoolean(Constants.IS_SORT_MOTTOS_BY_RANDOM, true).apply()
+        }
+    }
 }
