47d6b186bded33a3e5ece271653160dd07bace10
karchx <karch.andrus@gmail.com>
2021-08-01 23:34:58 +0300

    Init Notes fragment

---
+++ app/src/main/java/github/karchx/motto/views/notes/NotesFragment.kt
@@ -0,0 +1,49 @@
+package github.karchx.motto.views.notes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import github.karchx.motto.databinding.FragmentNotesBinding
+import github.karchx.motto.viewmodels.notes.NotesViewModel
+import github.karchx.motto.views.MainActivity
+import github.karchx.motto.views.tools.managers.Arrow
+
+
+class NotesFragment : Fragment() {
+
+    private lateinit var notesViewModel: NotesViewModel
+
+    private var _binding: FragmentNotesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        notesViewModel = ViewModelProvider(this).get(NotesViewModel::class.java)
+
+        _binding = FragmentNotesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Arrow.hideBackArrow(activity as MainActivity)
+        initData()
+        initViews()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initData() {
+    }
+
+    private fun initViews() {
+    }
+}
