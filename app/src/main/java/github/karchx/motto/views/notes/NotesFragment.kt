d350bccd4fc12e376aa4074dcd93fbb8812ca4ad
karchx <karch.andrus@gmail.com>
2021-08-02 17:21:08 +0300

    Added bottom sheet behavior

--- app/src/main/java/github/karchx/motto/views/notes/NotesFragment.kt
+++ app/src/main/java/github/karchx/motto/views/notes/NotesFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import github.karchx.motto.databinding.FragmentNotesBinding
 import github.karchx.motto.viewmodels.notes.SavedNotesViewModel
 import github.karchx.motto.views.MainActivity
@@ -15,6 +18,7 @@ import github.karchx.motto.views.tools.managers.Arrow
 class NotesFragment : Fragment() {
 
     private lateinit var notesViewModel: SavedNotesViewModel
+    private lateinit var notesBottomSheet: BottomSheetBehavior<FrameLayout>
 
     private var _binding: FragmentNotesBinding? = null
     private val binding get() = _binding!!
@@ -44,5 +48,10 @@ class NotesFragment : Fragment() {
     }
 
     private fun initViews() {
+        notesBottomSheet = BottomSheetBehavior.from(binding.notesBottomSheet).apply {
+            peekHeight = 100
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+            isHideable = true
+        }
     }
 }
