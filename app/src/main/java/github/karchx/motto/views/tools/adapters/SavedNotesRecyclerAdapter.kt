c76086345ae1f56e1c2651c995ce5c16c5dd03b9
karchx <karch.andrus@gmail.com>
2021-08-03 10:37:17 +0300

    Added notes recycler adapter

--- app/src/main/java/github/karchx/motto/views/tools/adapters/SavedNotesRecyclerAdapter.kt
+++ app/src/main/java/github/karchx/motto/views/tools/adapters/SavedNotesRecyclerAdapter.kt
@@ -1,4 +1,54 @@
 package github.karchx.motto.views.tools.adapters
 
-class SavedNotesRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import github.karchx.motto.R
+import github.karchx.motto.models.db.user_notes.UserNote
+import github.karchx.motto.models.storages.Constants
+
+class SavedNotesRecyclerAdapter(
+    private val notes: ArrayList<UserNote>
+) : RecyclerView.Adapter<SavedNotesRecyclerAdapter.SavedNotesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedNotesViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.motto_item, parent, false)
+        return SavedNotesViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SavedNotesViewHolder, position: Int) {
+        val noteTitle = getNoteTitle(notes[position].quote)
+        val noteSource = notes[position].source
+        val noteDateSaved = notes[position].dateSaved
+
+        holder.noteTitle.text = noteTitle
+        holder.noteSource.text = noteSource
+        holder.noteDateAdded.text = noteDateSaved
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    private fun getNoteTitle(quote: String): String {
+        val noteTitleWordsArray = quote.split(" ")
+
+        if (noteTitleWordsArray.size > Constants.QUANTITY_WORDS_IN_MOTTO_TITLE) {
+            val cutNoteTitleWordsArray =
+                noteTitleWordsArray.take(Constants.QUANTITY_WORDS_IN_MOTTO_TITLE)
+            var noteTitle = cutNoteTitleWordsArray.joinToString(separator = " ")
+            noteTitle = "$noteTitle <...>"
+            return noteTitle
+        } else {
+            return quote
+        }
+    }
+
+    class SavedNotesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val noteTitle: TextView = itemView.findViewById(R.id.textview_note_title)
+        val noteSource: TextView = itemView.findViewById(R.id.textview_note_source)
+        val noteDateAdded: TextView = itemView.findViewById(R.id.textview_note_time_added)
+    }
 }
