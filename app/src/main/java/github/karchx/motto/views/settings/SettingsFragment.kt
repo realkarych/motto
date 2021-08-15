117dd2e318548213ff09b068d2f2664b15aebeb4
karchx <karch.andrus@gmail.com>
2021-08-15 23:25:29 +0300

    Code refactored

--- app/src/main/java/github/karchx/motto/views/settings/SettingsFragment.kt
+++ app/src/main/java/github/karchx/motto/views/settings/SettingsFragment.kt
@@ -63,9 +63,19 @@ class SettingsFragment : Fragment() {
 
         buttonRateApp.setOnClickListener {
             try {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=$packageName")
+                    )
+                )
             } catch (e: ActivityNotFoundException) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$packageName")))
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+                    )
+                )
             }
         }
     }
